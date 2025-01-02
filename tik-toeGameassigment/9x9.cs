1c44a564f06972f1de120a076c68a4ab9b2de345
amir hossein naseh <135824015+zomirex@users.noreply.github.com>
2025-01-02 14:22:21 +0300

    9x9 almost completed

--- tik-toeGameassigment/9x9.cs
+++ tik-toeGameassigment/9x9.cs
@@ -10,10 +10,11 @@ namespace tik_toeGameassigment
     {
         class Player
         {
+
             static void Main(string[] args)
             {
-                char[,] board = new char[9, 9]; // 9x9 board representation
-                Dictionary<int, char> turns = new Dictionary<int, char>();
+                char[,] board = new char[9, 9]; // 9x9 board 
+                //Dictionary<int, char> turns = new Dictionary<int, char>();
 
 
                 while (true)
@@ -39,8 +40,16 @@ namespace tik_toeGameassigment
                         int col = int.Parse(inputs[1]);
                         board[row, col] = '.';
                     }
+                    for (int i = 0;i < 9;i++)
+                    {
+                        if (IsBoardFull(board, i)==false)
+                        {
+
+                        }
+                    }
                 }
             }
+            //tagsim kardan board 
             static (int,int) dividedBoard(int num)
             {
                 
@@ -70,11 +79,108 @@ namespace tik_toeGameassigment
                 }
                 
             }
-            static bool IsEmptyBoard(List<(int,int)> validActions)
+            // chek kardan barande baray mini bardaha
+            static int CheckWinner(char[,] board,(int row,int col)map)
+            {
+                // ofoghi
+                for (int i = map.row; i < 3+map.row; i++)
+                {
+                    if (board[i, map.col] == board[i, map.col+1] && board[i, map.col+1] == board[i, map.col+2])
+                    {
+                        return board[i, map.col + 0] == 'X' ? 1 : -1;
+                    }
+                }
+
+                // amoodi
+                for (int i = map.col; i < 3+ map.col; i++)
+                {
+                    if (board[map.row, i] == board[map.row + 1, i] && board[map.row + 1, i] == board[map.row + 2, i])
+                    {
+                        return board[map.row + 0, i] == 'X' ? 1 : -1;
+                    }
+                }
+
+                // movarab
+                if (board[map.row+0, map.col + 0] == board[map.row + 1, map.col + 1] && board[map.row + 1, map.col + 1] == board[map.row + 2, map.col+2] ||
+                    board[map.row + 0, map.col + 2] == board[map.row + 1, map.col + 1] && board[map.row + 1, map.col + 1] == board[map.row + 2, map.col + 0])
+                {
+                    return board[map.row + 1, map.col + 1] == 'X' ? 1 : -1;
+                }
+
+                return 0;
+            }
+            // dorost kardan ye map as main board ?* camel nist
+            static char[,]? MainBoardState(int mapnum,bool iswin, char[,] map)
+            {
+                return null;
+            }
+            // kamel boodan board
+            static bool IsBoardFull(char[,] board,int num)
+            {
+                (int row, int col) = dividedBoard(num);
+                for (int i = row; i < 3+row; i++)
+                {
+                    for (int j = col; j < col+3; j++)
+                    {
+                        if (board[i,j]=='.')
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            //algorythem mini max
+            static int Minimax(char[,] board, int depth, bool isMaxturn,int num)
             {
 
-                
-                return true;
+                int winner = CheckWinner(board,dividedBoard(num));
+                if (winner != 0)
+                {
+                    return winner == 1 ? -1 : 1;
+                }
+
+                if (IsBoardFull(board,num))
+                {
+                    return 0;
+                }
+
+                if (isMaxturn)//my turn
+                {
+                    int bestScore = int.MinValue;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (board[i, j] == '.')
+                            {
+                                board[i, j] = 'X';// do
+                                int score = Minimax(board, depth + 1, false,num);
+                                board[i, j] = '.'; // Undo
+                                bestScore = Math.Max(score, bestScore);
+                            }
+                        }
+                    }
+                    return bestScore;
+                }
+                else//opp turn 
+                {
+                    int bestScore = int.MaxValue;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (board[i, j] == '.')
+                            {
+                                board[i, j] = 'O';// Do 
+                                int score = Minimax(board, depth + 1, true, num);
+                                board[i, j] = '.'; // Undo 
+                                bestScore = Math.Min(score, bestScore);
+                            }
+                        }
+                    }
+                    return bestScore;
+                }
             }
 
         }
